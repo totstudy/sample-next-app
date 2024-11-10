2edc565e92bd96a680f986cab95c1ffcd7ac10e7
torutakahashi <5656a7green@gmail.com>
2024-11-10 19:46:19 +0900

    初回コミット

---
+++ README.md
@@ -0,0 +1,31 @@
+
+TypeScriptとReactとNext.jsで作成したWEBアプリケーションです。
+
+## インストール
+
+```bash
+npm install
+```
+
+## 環境変数の設定
+
+.envを作成
+
+```
+API_BASE_URL=<バックエンドAPIへのベースURLの設定>
+NEXT_PUBLIC_API_BASE_PATH=/api/proxy
+```
+
+## 開発サーバー起動
+
+JSON Serverの設定と起動
+
+
+こちらのリポジトリとは別のディレクトリで作業する
+
+開発サーバーを起動し、http://localhost:3000/ にアクセス
+
+```
+npm run dev
+```
+
---
+++ src/components/organisms/Footer/index.tsx
@@ -0,0 +1,65 @@
+import Link from 'next/link'
+import styled from 'styled-components'
+import Text from 'components/atoms/Text'
+import Box from 'components/layout/Box'
+import Flex from 'components/layout/Flex'
+
+const Anchor = styled(Text)`
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+/**
+ * フッター
+ */
+const Footer = () => {
+  return (
+    <footer>
+      <Flex flexDirection={{ base: 'column', md: 'row' }}>
+        <Box
+          minWidth={{ base: '100%', md: '120px' }}
+          paddingRight={{ base: 0, md: 1 }}
+        >
+          <nav>
+            <Box marginBottom={2}>
+              <Link href="/" passHref>
+                <Anchor as="a">トップ</Anchor>
+              </Link>
+            </Box>
+            <Box marginBottom={2}>
+              <Link href="/" passHref>
+                <Anchor as="a">お知らせ</Anchor>
+              </Link>
+            </Box>
+          </nav>
+        </Box>
+        <Box
+          minWidth={{ base: '100%', md: '120px' }}
+          paddingRight={{ base: 0, md: 1 }}
+        >
+          <nav>
+            <Box marginBottom={2}>
+              <Link href="/" passHref>
+                <Anchor as="a">利用規約</Anchor>
+              </Link>
+            </Box>
+            <Box marginBottom={2}>
+              <Link href="/" passHref>
+                <Anchor as="a">プライバシーポリシー</Anchor>
+              </Link>
+            </Box>
+            <Box marginBottom={2}>
+              <Link href="/" passHref>
+                <Anchor as="a">配送と返品</Anchor>
+              </Link>
+            </Box>
+          </nav>
+        </Box>
+      </Flex>
+    </footer>
+  )
+}
+
+export default Footer
